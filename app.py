b8df88203387690b264fd81c7eabf5c0a754348b
riojajosue6-boop <riojajosue6@gmail.com>
2026-05-21 12:36:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -50,22 +50,7 @@ RECURSOS_BASE = [
         "categoria": "pdf",
         "badge": "📖 GUÍA TEXTO",
         "titulo": "COMO_VENDER_CUANDO_NADIE_ESTA_COMPRANDO_2",
-        "descripcion": "Si últimamente lo único que escuchas es
-                        que la gente no está comprando porque no
-                        hay forma de hacerlo cuando el mundo está
-                        parado entonces debes replantearte
-                        inmediatamente la situación. Como
-                        emprendedores o líderes de proyectos
-                        debemos tomar estos retos con pasión para
-                        tratar de generar nuevos ángulos de
-                        negocios.
-
-                        Esta guía te servirá como un primer paso
-                        para redireccionar los pasos que deberás
-                        dar y justamente es lo que necesitamos en
-                        este momento, un modelo que ayude a
-                        nuestra mente a entender cómo abordar la
-                        situación.",
+        "descripcion": "Si últimamente lo único que escuchas es que la gente no está comprando porque no hay forma de hacerlo cuando el mundo está parado entonces debes replantearte inmediatamente la situación. Como emprendedores o líderes de proyectos debemos tomar estos retos con pasión para tratar de generar nuevos ángulos de negocios. Esta guía te servirá como un primer paso para redireccionar los pasos que deberás dar y justamente es lo que necesitamos en este momento, un modelo que ayude a nuestra mente a entender cómo abordar la situación.",
         "icono": "https://images.unsplash.com/photo-1506880018603-83d5b814b5a6?w=400&auto=format&fit=crop&q=80",
         
         # 🎯 NOTA LA DIFERENCIA: Este enlace termina en /preview para activar el modo lectura
